2fec167a6c9d0cf513e343775ca82937026c9a8c
Naveen Kumar <naveenkatoch61@gmail.com>
2025-06-08 09:32:13 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
+/* Episode 1
+ const heading = React.createElement(
   "h1",
   { id: "heading", xyz: "abc" },
-  "Hello World! from React."
+  "Hello Naveen! from React."
 );
 
 const parent = React.createElement("div", { id: "parent" }, [
@@ -23,3 +24,36 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
 
 // This above is example of using react without JSX
+*/
+
+//JSX in html like syntax
+const Title = function () {
+  return (
+    <h1 id="head" className="head" tabIndex="1">
+      Namaste React using JSX
+    </h1>
+  );
+};
+
+const number = 10000;
+
+const title = <h1 className="title">Namaste Reast</h1>;
+
+// we can omit return and {}
+//Capital case to start a React component name
+const JsxHeadingComponent = () => {
+  return (
+    <div id="container">
+      <h2>{number}</h2>
+      <Title />
+      {Title()}
+      {title}
+      <h1 id="heading" className="heading" tabIndex="1">
+        Namaste React in Functional Component
+      </h1>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<JsxHeadingComponent />);
